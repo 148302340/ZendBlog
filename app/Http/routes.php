244cef7c121713f060856b49a2572201f435f6af
fozhu <512355793@qq.com>
2017-03-27 09:51:58 +0800

    一次提交

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,5 +15,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::controller("/home","home\centerController");
+// 前台个人中心
+Route::controller('/home/center','home\CenterController');
 
+// 后台用户路由
+Route::controller('/admin/user','admin\UserController');
+
+// 后台栏目路由
+Route::controller('/admin/cate','admin\CateController');
+
+// 后台文章路由
+Route::controller('/admin/para','admin\BlogController');
+
+// 后台友情链接路由
+Route::controller('/admin/friendlink','admin\LinkController');
+
+Route::get('/admin/web/set',function(){
+	return view('admin.web.set');
+});
